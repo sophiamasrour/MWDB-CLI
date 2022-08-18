b9abb630759a9f4c8113002f87eb5ef613e10291
Sophia Masrour <sophiamasrour@CodeHunters-MacBook-Pro.local>
2022-08-18 12:49:25 -0400

    added --dirr arg

--- cli.py
+++ cli.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+import magic
 from mwdblib import MWDB
 
 class CLI(MWDB):
@@ -9,7 +10,12 @@ class CLI(MWDB):
 
         from scripts import batch_upload
         try:
+            #print('path: ' + path)
             paths = batch_upload.get_samples_path(path)
+            print(len(paths))
+            for each in paths:
+                print(each)
+            #print('tags: ' + tags)
             batch_upload.upload_files(paths, tags)
 
         except Exception as m:
@@ -34,22 +40,22 @@ parser = argparse.ArgumentParser(
 parser.add_argument('-u', nargs='+', help='Upload sample')
 parser.add_argument('-r', nargs=1, help='Run full reanalysis on all matched samples')
 parser.add_argument('--last-two', nargs='+', type=str, help=' Retrieve results from the latest two Karton analyses instead of one')
-parser.add_argument('--dir')
 parser.add_argument('--query', nargs=1, help='Lucene query')
-#parser.add_argument('--dir', help='Path to directory of samples to be uploaded')
+parser.add_argument('--dirr', nargs=1, help='Path to directory of samples to be uploaded')
 
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    cli = CLI()
+    cli = CLI(api_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJsb2dpbiI6InNvcGhpYSIsImFwaV9rZXlfaWQiOiIwOTA4MzI2MS00NDhmLTQwOWItYjc5MS1hNDhjODBmYjZiYmIiLCJpYXQiOjE2NTc3Mjg4NDIsImF1ZCI6Imh0dHA6Ly8wLjAuMC4wOjUwMDAiLCJzY29wZSI6ImFwaV9rZXkiLCJzdWIiOiJzb3BoaWEifQ.HsmGFocjW-wVOWGXMngzIoY5pzfNP4DBFebfu_i7a4vewFtHOG7CeboLNrrjNqBecJlUg3yGOEMTx8bDu3NxkQ', api_url = 'https://mwdb-development.labs:5000/api/', verify_ssl = False)
     if args.u:
-        if args.dir:
-            path = args.dir
-            cli.batch_upload(args.u[0], path)
+        if args.dirr:
+            #print('dir: ' + args.dirr[0])
+            #print('name: ' + args.u[0])
+            cli.batch_upload(args.u[0], args.dirr[0])
         else:
             cli.batch_upload(args.u[0])
 
-
+ 
     elif args.r:
         cli.batch_reanalysis(args.r[0])
 
